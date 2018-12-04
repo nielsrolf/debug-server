948ca7867e66014b632e61b7ae5618a8a14b71e4
Niels Warncke <nielswarncke@Julian.local>
2018-12-04 13:34:47 +0100

    works

---
+++ README.md
@@ -0,0 +1,9 @@
+# Request Log Server
+Show every request that gets send to the server - useful for debugging, if external libraries generate requests that you want to inspect.
+
+## Get it running
+To get it running, you need to have Python and Flask installed. Then:
+ ```
+ >> python server.py
+ ```
+ It runs on port 4200.
---
+++ server.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, Response
+import pprint as pp
+
+
+app = Flask(__name__)
+
+def request_params():
+    if request.method == 'GET':
+        return request.args.to_dict()
+    else:
+        return request.get_json(force=True)
+
+@app.route('/', defaults={'path': ''}, methods=['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'])
+@app.route('/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'])
+def log_request(path="/"):
+    print("----- NEW REQUEST ------")
+    print(request.method, path)
+    print(">> HEADERS: ")
+    pp.pprint(request.headers)
+    print(">> BODY: ")
+    pp.pprint(request_params())
+    return Response(status=204)
+
+if __name__ == '__main__':
+      app.run(host='0.0.0.0', port=4200)
